5f7bb0524b9b5d058f57e6dd10b5b74305dee24a
Priyanshu Chauhan <152474798+piyushxt09@users.noreply.github.com>
2025-03-02 12:03:40 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/logout" element={<Logout />} />
