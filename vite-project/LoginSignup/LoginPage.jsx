870c6376ca53c9d83e025115e020d8437af27168
Priyanshu Chauhan <152474798+piyushxt09@users.noreply.github.com>
2025-03-02 11:47:21 +0530

    Update LoginPage.jsx

--- vite-project/LoginSignup/LoginPage.jsx
+++ vite-project/LoginSignup/LoginPage.jsx
@@ -28,7 +28,7 @@ export default function LoginPage() {
         }
 
         try {
-            const response = await fetch("http://localhost:8081/api/login", {
+            const response = await fetch("https://drone-website-backend-aeqq.onrender.com/api/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
