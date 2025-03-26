e30582cb61c7f205337b000c7b7a03dce7fa70f0
piyushxt09 <chauhanluckey4@gmail.com>
2025-03-26 20:01:54 +0530

    udated

--- vite-project/src/CategorySingle.jsx
+++ vite-project/src/CategorySingle.jsx
@@ -6,9 +6,9 @@ import Footer from '../components/Footer';
 export default function CategorySingle() {
   return (
     <>
-      < Header />
-      < Category />
-      < Footer />
+      <Header />
+      <Category />
+      <Footer />
     </>
   )
 }
