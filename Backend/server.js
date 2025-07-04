badbf939714ea72d60e42460fd92df768ca969da
Priyanshu Chauhan <152474798+piyushxt09@users.noreply.github.com>
2025-07-04 13:08:36 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 // Start server on dynamic port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
